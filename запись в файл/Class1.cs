6a33a10a46bfee6f2c6b6a056ab75dd3cf023ea1
Андрей <nevraev1109@mail.ru>
2022-08-29 18:13:10 +0500

    Add project files.

---
+++ запись в файл/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MultiFilesAndDelegates
+{
+    class myClass
+    {
+        static private string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            , filename;
+
+       
+        static public void WriteHello(string Name, string FileName)
+        {
+            string _text = "Привет, " + Name + "!" + "\n";
+            path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            filename = path + "\\" + FileName;
+            StreamWriter sw = File.AppendText(filename);
+            sw.WriteLine(_text);
+            sw.Close();
+        }
+        static public string ReadTaskFile(string fileName)
+        {
+            string _input = "";
+            int counter = 0;
+            fileName = path + "\\" + fileName;
+            StreamReader sr = new StreamReader(fileName);
+
+            _input = sr.ReadLine();
+            return _input;
+
+            /*while ( sr.ReadLine() != null)
+            {
+                _input = sr.ReadLine();
+                counter++;
+            }
+            return _input;*/
+
+        }
+    }
+}
+
---
+++ запись в файл/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Data;
+
+namespace MultiFilesAndDelegates
+{
+    delegate void myDelegate(string myStr, string tmp);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           // Regex regex = new Regex(@"(\d+)", RegexOptions.IgnoreCase);
+            Regex regex = new Regex(@"(\S)", RegexOptions.IgnoreCase);
+            MatchCollection matches = regex.Matches(myClass.ReadTaskFile("Journal.txt"));
+            // MatchCollection matches = Regex.Matches("", @"(\D)");
+
+            string x = "";
+            
+            if (matches.Count != null)
+            {
+                
+                for (int i = 0; i < matches.Count; i++)
+                    
+                {
+
+
+
+                    /* string a = matches[i].ToString();
+                     string s = matches[0].ToString();
+                     int S = int.Parse(s);
+                     string z = matches[2].ToString();
+                     int Z = int.Parse(z);*/
+
+
+                    x += matches[i].ToString();
+                   
+
+                }
+
+                double rez = Convert.ToDouble(new DataTable().Compute(x, ""));
+                Console.WriteLine(rez);
+            }
+            else
+            {
+                Console.WriteLine("Совпадений не найдено");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
